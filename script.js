b71ccb5db951ec136cf949d5f5176049c496e7b3
JHale3 <learnspark3@gmail.com>
2024-02-09 11:32:55 -0500

    cell event listener + some placement logic

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ function renderGameBoard(gameBoard, board) {
   boardElement.innerHTML = ''; 
   for (let row = 0; row < 10; row++) {
     for (let col = 0; col < 10; col++) {
+      const cellId = `cell-${row}-${col}`;
       const cell = document.createElement('div');
+      cell.id = cellId;
       cell.className = 'cell';
       cell.textContent = `${String.fromCharCode(65 + row)}${col + 1}`;
       boardElement.appendChild(cell);
@@ -19,13 +21,33 @@ renderGameBoard('playerBoard');
 renderGameBoard('computerBoard');
 
 //variables
-const selectedShip = null;
-const shipDirection = 'horizontal';
+let selectedShip = null;
+//NEED TO ADD ROTATION//
+let shipDirection = 'vertical';
 
-const ships = document.querySelectorAll('ship');
+const ships = document.querySelectorAll('.ship');
 //event listener for ships//
 ships.forEach(ship => {
-    ship.addEventListener('click' function() {
+    ship.addEventListener('click', function() {
         selectedShip = this;
+    })
+})
+
+const cells = document.querySelectorAll('.cell');
+cells.forEach(cell => {
+    cell.addEventListener('click', function() {
+        if (selectedShip) {
+            const [row, col] = cellId.split('-').slice(1).map(Number);
+            const shipSize = parseInt(selectedShip.dataset.size);
+            if(canPlaceShip(row, col, shipSize, shipDirection)) {
+                placeShip(row, col, shipSize, shipDirection)
+                this.appendChild(selectedShip);
+
+                selectedShip = null;
+            } else {
+                alert('Invalid cordinates!, please choose another location')
+            }
+
+        }
     })
 })
